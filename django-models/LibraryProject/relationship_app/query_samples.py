83bdf33c547057d9e3659f73794282d294d31e38
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-09-22 15:56:07 +0100

    add query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,12 +3,16 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Sample queries demonstrating relationships
 def sample_queries():
     # Query all books by a specific author
-    author_books = Book.objects.get()
-    print(f"Books by {Author.name}: {[book.title for book in author_books]}")
+    author_books = Book.objects.get(author = 'author_name')
+    His_books = author_books.books.all()
+    print(His_books)
+    
+    #print(f"Books by {Author.name}: {[book.title for book in author_books]}")
 
     #Retrieve all books in a library
-    book = Library.objects.get(name=(Library.name), books.all())
-    print(book)
+    book = Library.objects.get(name='Library_name')
+    books_in_library = book.books.all()
+    print(books_in_library)
 
     #Retrieve the librarian for a library
     lib = library.objects.first()
