7d65e68dab46f64d5a1ed2148496e6e2e2973fab
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-09-24 16:00:32 +0100

    query samples update

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,18 +3,23 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Sample queries demonstrating relationships
 def sample_queries():
     # Query all books by a specific author
-    author_books = Book.objects.get(author=author_name)
-    His_books = author_books.books.all()
-    print(His_books)
+    def get_books_by_author(author_name):
+    # Fetch the author object by name
+        author = Author.objects.get(name=author_name)  
+
+    # Get all books written by this author
+        books = Book.objects.filter(author=author)  
+    
+        return books
     
     #print(f"Books by {Author.name}: {[book.title for book in author_books]}")
 
     #Retrieve all books in a library
-    book = Library.objects.get(name=library_name)
-    books_in_library = book.books.all()
-    print(books_in_library)
+book = Library.objects.get(name='library_name')
+books_in_library = book.books.all()
+print(books_in_library)
 
     #Retrieve the librarian for a library
-    lib = Library.objects.first()
-    lib_librarian = lib.librarian
-    print(f"Librarian of {lib.name}: {lib_librarian.name}")
+lib = Library.objects.first()
+lib_librarian = lib.librarian
+print(f"Librarian of {lib.name}: {lib_librarian.name}")
