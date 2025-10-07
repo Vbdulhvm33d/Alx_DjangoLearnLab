8b45f9bea5980aa3b888a2a18fe1a454f9c75b07
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-10-07 11:51:14 +0100

    add viewsets and routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,8 +2,13 @@
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
